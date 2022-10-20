d1d1e01803430bc89fb5b594f587be8b184b8148
hackern0v1c3 <kev@hackernovice.com>
2022-10-19 22:09:07 -0400

    added script for lab on wsl network

---
+++ Hyperv-WslNetwork-2Dcs-1Win11.ps1
@@ -0,0 +1,38 @@
+$networkName = "WSL"
+
+$dc1Parameters = @{
+    Name            = 'DC1'
+    OperatingSystem = 'Windows Server 2022 Standard Evaluation (Desktop Experience)'
+    Network         = $networkName
+    Roles           = 'RootDC'
+    DomainName      = 'TestLab.local'
+}
+
+$dc2Parameters = @{
+    Name            = 'DC2'
+    OperatingSystem = 'Windows Server 2022 Standard Evaluation (Desktop Experience)'
+    Network         = $networkName
+    Roles           = 'DC'
+    DomainName      = 'TestLab.local'
+}
+
+$ws1Parameters = @{
+    Name            = 'WS1'
+    OperatingSystem = 'Windows 11 Enterprise Evaluation'
+    Network         = $networkName
+    DomainName      = 'TestLab.local'
+}
+
+New-LabDefinition -Name $networkName -DefaultVirtualizationEngine HyperV
+
+Set-LabInstallationCredential -Username 'adminaccount' -Password 'P@ssword123!'
+Add-LabDomainDefinition -Name TestLab.local -AdminUser 'adminaccount' -AdminPassword 'P@ssword123!'
+Add-LabVirtualNetworkDefinition -Name $networkName -HyperVProperties @{ SwitchType = 'Internal' }
+
+Add-LabMachineDefinition @dc1Parameters
+Add-LabMachineDefinition @dc2Parameters
+Add-LabMachineDefinition @ws1Parameters
+
+Install-Lab
+
+Show-LabDeploymentSummary -Detailed
